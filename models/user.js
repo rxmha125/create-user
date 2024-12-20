e782a243a8ae64493bdf550b29f7121211d15399
rxmha125 <rx.mha2@gmail.com>
2024-12-21 03:29:05 +0600

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/testapp1");
+// Use environment variable for connection string (for local or production)
+mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/testapp1", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.log('MongoDB connection error: ', err));
 
 const userSchema = mongoose.Schema({
     image: String,
     email: String,
     name: String
-})
+});
 
 module.exports = mongoose.model('user', userSchema);
